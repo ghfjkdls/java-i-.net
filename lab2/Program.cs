3445e71a2568117a9709a06e8228e7fe3e40d8e0
ghfjkdls <61516241+ghfjkdls@users.noreply.github.com>
2021-03-26 12:09:38 +0100

    Add files via upload

---
+++ lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            load();
+            Console.Read();
+        }
+
+        public static async void load(){
+            string call = "https://openexchangerates.org/api/latest.json?app_id=72a8ed46e61f49c09af2017a0c3dd6a5&symbols=PLN,EUR,GBP";
+            HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync(call);
+
+
+            Console.WriteLine(response);
+        }
+    }
+}
