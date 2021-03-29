11a7257c63ed606684de4d28a7b715f6e2a2f6eb
ghfjkdls <61516241+ghfjkdls@users.noreply.github.com>
2021-03-29 11:52:06 +0200

    Add files via upload

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -12,12 +12,19 @@ namespace lab2
         }
 
         public static async void load(){
-            string call = "https://openexchangerates.org/api/latest.json?app_id=72a8ed46e61f49c09af2017a0c3dd6a5&symbols=PLN,EUR,GBP";
-            HttpClient client = new HttpClient();
-            string response = await client.GetStringAsync(call);
-
-
-            Console.WriteLine(response);
+            Console.WriteLine("wpisz rok");
+            //string year = Console.ReadLine();
+            int year = Convert.ToInt32(Console.ReadLine());
+            if(year > 2021) return;
+            int n = 2021 -year + 1;
+            for(int i = 0; i < n; i++){
+                string call = "https://openexchangerates.org/api/historical/" + year.ToString() + "-01-01.json?app_id=72a8ed46e61f49c09af2017a0c3dd6a5&symbols=PLN,EUR,GBP";
+                HttpClient client = new HttpClient();
+                string response = await client.GetStringAsync(call);
+                Console.WriteLine(response);
+                Console.WriteLine(year);
+                year++;
+            }
         }
     }
 }
